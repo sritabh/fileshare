7f9eb2894db344b0faf5f87833fb370340791355
SobyDamn <sobydanny@gmail.com>
2020-04-13 23:21:10 +0530

    ADDED -auto connection to server

--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,6 @@
-function loadContent() {
-    console.log("HEllo");
-    var xhttp;
+async function loadContent() {
+  var bodyContent = document.getElementById("body").innerHTML
+  var xhttp;
   if (window.XMLHttpRequest) {
     // code for modern browsers
     xhttp = new XMLHttpRequest();
@@ -8,14 +8,21 @@ function loadContent() {
     // code for IE6, IE5
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
-  
-  xhttp.onreadystatechange = function() {
-    console.log(xhttp.status);
+  await xhttp.open("GET", "http://192.168.43.1:8080", true);
+  await xhttp.send()
+  xhttp.onreadystatechange = async function() {
     if (this.status == 200) {
       document.getElementById("body").innerHTML =
       this.responseText;
+      console.log("COnnedcted");
+    }
+    else {
+      document.getElementById("body").innerHTML = bodyContent;
+      ///Keep asking for the connection till we recieve one
+      setTimeout(()=>{
+        loadContent();
+      },5000);
+      console.log("No Connetcion");
     }
   };
-  xhttp.open("GET", "http://192.168.43.1:8080", true);
-  xhttp.send();
 }
