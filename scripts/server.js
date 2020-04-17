08b79c7eedc77c05592655d523e3785300ac6909
SobyDamn <sobydanny@gmail.com>
2020-04-17 14:29:38 +0530

    back to http html server

--- scripts/server.js
+++ scripts/server.js
@@ -12,7 +12,7 @@ async function loadContent() {
   await xhttp.send()
   xhttp.onreadystatechange = async function() {
     if (this.status == 200) {
-      //document.getElementById("body").innerHTML = this.responseText;
+      document.getElementById("body").innerHTML = this.responseText;
       console.log("COnnedcted");
     }
     else {
@@ -25,46 +25,3 @@ async function loadContent() {
     }
   };
 }
-
-let socket = new WebSocket("ws://192.168.43.1:8080");
-
-socket.onopen = function(e) {
-  console.log("[open] Connection established");
-  console.log("Sending to server");
-  //socket.send("My name is John");
-};
-
-socket.onmessage = function(event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-};
-
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
-  }
-};
-
-socket.onerror = function(error) {
-  console.log(`[error] ${error.message}`);
-};
-socket.onmessage = function(event) {
-  console.log(`[message] Data received from server: ${event.data}`);
-};
-
-socket.onclose = function(event) {
-  if (event.wasClean) {
-    console.log(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-  } else {
-    // e.g. server process killed or network down
-    // event.code is usually 1006 in this case
-    console.log('[close] Connection died');
-  }
-};
-
-socket.onerror = function(error) {
-  console.log(`[error] ${error.message}`);
-};
