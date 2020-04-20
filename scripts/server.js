8cd3ca7e793588df4bd8afcbbbf0844eb11b4143
SobyDamn <sobydanny@gmail.com>
2020-04-20 22:20:45 +0530

    back to http

--- scripts/server.js
+++ scripts/server.js
@@ -8,11 +8,31 @@ async function loadContent() {
     // code for IE6, IE5
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
-  await xhttp.open("GET", "http://192.168.43.1:8080", true)
-  await xhttp.send()
-  xhttp.onreadystatechange = async function() {
+  xhttp.open("GET", "http://192.168.43.1:8080", true)
+  xhttp.send();
+  //xhttp.responseType = 'blob';
+  xhttp.onreadystatechange = async function(data) {
+    console.log("ready state",this.readyState)
+    console.log(data)
     if (this.status == 200) {
       document.getElementById("body").innerHTML = this.responseText;
+      var myblob = new Blob([stringTobufferArray(this.response)])
+      await myblob;
+      console.log("response:-"+this.responseText.slice(0,80))
+      console.log("blob type "+myblob.type+"blob size "+myblob.size)
+      var getUrl = new Promise((res,rej)=>{
+        var url = window.URL.createObjectURL(myblob)
+        res(url)
+      });
+      getUrl.then((url)=>{
+        console.log("I got the url "+url)
+        document.getElementById("status").innerHTML = "<a href='"+url+"' download='merafile.pdf'>Got file</a><br>size:-"+myblob.size/(1024*1024)
+        document.getElementById("status").onclick = function (ev) {
+          setTimeout(()=>{
+            window.URL.revokeObjectURL(url);
+          },5000);
+        }
+      })
       console.log("COnnedcted");
     }
     else {
@@ -24,4 +44,26 @@ async function loadContent() {
       console.log("No Connetcion");
     }
   };
+};
+function stringTobufferArray(string) {
+  console.log("Processing file....");
+  var buf = new ArrayBuffer(string.length);
+  var bufView = new Uint8Array(buf);
+  for (var i=0,strLen = string.length;i<strLen;i++) {
+    bufView[i] = string.charCodeAt(i);
+  };
+  console.log(bufView.length)
+  return buf;
+}
+function stringToUint(string) {
+  var uInt8Array = new Uint8Array(string);
+    var i = uInt8Array.length;
+    var binaryString = new Array(i);
+    while (i--)
+    {
+      binaryString[i] = String.fromCharCode(uInt8Array[i]);
+    }
+    var data = binaryString.join('');
+    console.log(data.length)
+  return data;
 }
