2e2783985d5c5c9cc0fd8f91c75ff683b206f2c6
SobyDamn <sobydanny@gmail.com>
2020-04-26 16:08:24 +0530

    test

--- scripts/StreamSaver.js
+++ scripts/StreamSaver.js
@@ -115,7 +115,7 @@
       if (!mitmTransporter) {
         mitmTransporter = isSecureContext
           ? makeIframe(streamSaver.mitm)
-          : makePopup(streamSaver.mitm)
+          : makeIframe(streamSaver.mitm)
       }
     }
   
